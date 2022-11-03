1e7b4a51b881e22f4fc73bece2496b2234ca7296
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-03 22:47:20 +0530

    Update create.js

--- splitit/pages/create.js
+++ splitit/pages/create.js
@@ -11,26 +11,42 @@ export default function create() {
     <>
     <Navbar />
     <div className='flex justify-center items-center'>
+
         <main className='my-16 rounded-3xl bg-gradient-to-r from-purple-500 to-pink-500 text-purple-500'>
+        <div className='flex justify-center items-center my-6 mx-6'>
+        <div className='rounded-2xl bg-slate-900'>
+        <div className=' text-3xl my-4 mx-8 cursor-pointer font-bold'>
+        Create Custom Room
+        </div>
+        </div>
+        </div>
+        
+        <div className=" p-4 mx-8 right flex flex-col">
+
+    
+    
+        <input className="px-8 h-12 my-2  rounded-lg  bg-slate-900 text-purple-500" type="text" placeholder="Room Name">
+    </input>
+    <input className="px-8 h-12 my-2  rounded-lg  bg-slate-900 text-purple-500" type="text" placeholder="Room Type">
+    </input>
+    </div>
 
 
-        <div className='flex justify-center items-center my-3 mx-6'>
+
+
+
+
+
+
+
+
+        <div className='flex justify-center items-center my-5 mx-6'>
         <div className='rounded-2xl bg-slate-900'>
         <button className=' text-2xl my-3 mx-8 cursor-pointer font-bold'>
             Create
             </button>
             </div>
             </div>
-
-
-
-
-
-
-
-
-
-
 
 
         </main>
