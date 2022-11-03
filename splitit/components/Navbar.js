648a9cf72f1f25a4dac4b94d5b06d0a625bd668c
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-03 16:45:01 +0530

    Update Navbar.js

--- splitit/components/Navbar.js
+++ splitit/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
         </span>
         <ul className= 'px-2 py-3 flex space-x-9 mx-4'>
             <li className='cursor-pointer text-xl font-bold transition ease-in-out delay-50 hover:-translate-y-1'>Home</li>
-            <li className='cursor-pointer text-xl font-bold transition ease-in-out delay-50 hover:-translate-y-1'>Room</li>
+            <li className='cursor-pointer text-xl font-bold transition ease-in-out delay-50 hover:-translate-y-1'>My Room</li>
             <li className='cursor-pointer text-xl font-bold transition ease-in-out delay-50 hover:-translate-y-1'>Transactions</li>
             <li className='cursor-pointer text-xl font-bold transition ease-in-out delay-50 hover:-translate-y-1'>Settings</li>
 
