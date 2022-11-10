a98226f5110a649ba6824dc4b09fdd45cfca5014
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-11 00:01:04 +0530

    Update createdroom.js

--- splitit/pages/createdroom.js
+++ splitit/pages/createdroom.js
@@ -11,7 +11,7 @@ export default function createdroom() {
     <>
     <Navbar />
     <div className='flex'>
-   <main className='my-16 mx-8 rounded-3xl bg-black ring-1 ring-pink-500'>
+   <main className='my-16 mx-8 rounded-3xl bg-gradient-to-r from-purple-500 to-pink-500 text-purple-500 '>
         <div className='flex justify-center items-center my-6 mx-12 '>
         <div className='rounded-2xl bg-slate-900 '>
 
@@ -27,6 +27,9 @@ export default function createdroom() {
         placeholder="Member 2's  Wallet Address" />
         <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="text" 
         placeholder="Member 3's  Wallet Address" />
+        <button className='bg-slate-900 text-purple-500 items-center justify-center h-12 my-2 rounded-lg text-xl font-bold hover:bg-slate-800'>
+ Add Member +
+        </button>
 </div>
 
 
