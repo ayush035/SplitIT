7f01aa01280fef14fd7a8a1f8040640557e19e13
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-13 22:18:51 +0530

    Update createdroom.js

--- splitit/pages/createdroom.js
+++ splitit/pages/createdroom.js
@@ -7,16 +7,16 @@ import Footer from '../components/Footer'
 
 
 export default function createdroom() {
-    return (
-    <>
-    <Navbar />
-    <div className='flex'>
-   <main className='my-16 mx-16 rounded-3xl bg-gradient-to-r from-purple-500 to-pink-500 text-purple-500 '>
+return (
+<>
+<Navbar />
+<div className='flex'>
+<main className='my-16 mx-16 rounded-3xl bg-gradient-to-r from-purple-500 to-pink-500 text-purple-500 '>
         <div className='flex justify-center items-center my-6 mx-10 '>
         <div className='rounded-2xl bg-slate-900 '>
 
         <div className=' text-3xl my-4 mx-8 cursor-pointer font-bold text-purple-500'>
-      Add Group Members
+Add Group Members
         </div>
         </div>
         </div>
@@ -28,22 +28,22 @@ export default function createdroom() {
         <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="text" 
         placeholder="Member 3's  Wallet Address" />
         <button className='bg-slate-900 text-purple-500 items-center justify-center h-12 my-2 rounded-lg text-xl font-bold hover:bg-slate-800'>
- Add Member +
+Add Member +
         </button>
 </div>
 <div className='flex justify-center items-center my-5 mx-6'>
         <div className='rounded-xl bg-slate-900 hover:bg-slate-800'>
         <div className=' text-2xl my-3 mx-8 cursor-pointer font-bold '>
-           Mint Invitation
-            </div>
-            </div>
-            </div>
+        Mint Invitation
+        </div>
+        </div>
+        </div>
 
 
 
-   </main>
-   </div>
+</main>
+</div>
     {/* <Footer /> */}
-    </>
-    )
+</>
+)
 }
