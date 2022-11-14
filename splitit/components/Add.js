c2ca6c5e575ac648927322798d3977582b756b5c
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-14 19:31:58 +0530

    main

---
+++ splitit/components/Add.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+
+return (
+    <div>
+        <>
+        <input className="px-4 h-12 my-2 border border-1 border-gray-600 rounded-lg outline-blue-600 bg-white text-black" type="text" 
+        placeholder="Member 3's  Wallet Address" />
+        </>
+        </div>
+        )
