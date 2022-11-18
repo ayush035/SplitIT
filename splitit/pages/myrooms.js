3c3aeb9c60ce6fa336615ab62dc48d6a3b7a2651
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-18 20:54:10 +0530

    main

--- splitit/pages/myrooms.js
+++ splitit/pages/myrooms.js
@@ -12,6 +12,21 @@ export default function create() {
     <>
     <Navbar />
 
+    <div className='my-16 mx-16'>
+        <div className='flex'>
+            <Link href={'/room1'}>
+        <div className='rounded-3xl bg-slate-900 '>
+        <div className='flex justify-center items-center my-6 mx-10 '>
+        <div className='rounded-2xl bg-slate-900 my-4 '>
+        <div className=' text-3xl my-4 mx-8 cursor-pointer font-bold text-purple-500'>
+Banglore's Trip
+        </div>
+        </div>
+        </div>
+        </div>
+        </Link>
+        </div>
+        </div>
 
     </>
     )
