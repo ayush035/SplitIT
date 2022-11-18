252f23d40ad520107bb8c30c62027cf1d81abeaf
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-18 19:43:17 +0530

    main

---
+++ splitit/pages/transactions.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Link from 'next/link'
+
+
+
+export default function create() {
+    return (
+    <>
+    <Navbar />
+
+
+
+
+
+
+
+
+
+    
+    </>
+    )
+    
+}
