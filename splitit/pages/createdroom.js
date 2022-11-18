9033cf55172a19b2cdae8a03db300dab1cfd0643
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-18 19:29:02 +0530

    Update createdroom.js

--- splitit/pages/createdroom.js
+++ splitit/pages/createdroom.js
@@ -15,21 +15,22 @@ return (
 <div className='flex'>
 <main>
 <div className='grid grid-cols-2'>
-<div className='my-16 mx-16 rounded-3xl bg-gradient-to-r from-purple-500 to-pink-500 text-purple-500 '>
+<div className='my-16 mx-16 '>
+        <div className='rounded-3xl bg-gradient-to-r from-purple-500 to-pink-500 text-purple-500 '>
         <div className='flex justify-center items-center my-6 mx-10 '>
-        <div className='rounded-2xl bg-slate-900 '>
+        <div className='rounded-2xl bg-slate-900 my-4 '>
         <div className=' text-3xl my-4 mx-8 cursor-pointer font-bold text-purple-500'>
 Add Group Members
         </div>
         </div>
         </div>
 
-        <div className='flex  justify-end items-end'></div>
-        <div className=' text-xl my-24 mx-8 cursor-pointer font-bold text-white'>
+        <div className='flex  justify-end items-end my-24'>
+        <div className=' text-xl mx-8 cursor-pointer font-bold text-white my-4'>
                 Share This Room code to add members
-
+                </div>
         </div>
-
+        </div>
 </div>
 <div className="flex flex-col my-24 mx-36 justify-center">
 <Image
