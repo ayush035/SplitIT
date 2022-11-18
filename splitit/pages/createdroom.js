fbc7edad76228bdabfb62cf2d3b502804574b175
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-18 19:22:23 +0530

    Update createdroom.js

--- splitit/pages/createdroom.js
+++ splitit/pages/createdroom.js
@@ -2,14 +2,11 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
-import Add from '../components/Add'
 import group from '../public/group.png'
 
 
 export default function createdroom() {
 
-        const member = () => {Add}
 return (
 <>
 
