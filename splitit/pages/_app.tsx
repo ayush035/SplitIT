19e6a603a5a8c4fc029eb0b4f75701563564f39a
Ayush Gupta <75483021+ayush035@users.noreply.github.com>
2022-11-25 02:22:39 +0530

    main

--- splitit/pages/_app.tsx
+++ splitit/pages/_app.tsx
@@ -1,21 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { ThirdwebProvider } from "@thirdweb-dev/react";
 
 
 
 export default function App({ Component, pageProps }: AppProps) {
  
   return (
-    <ThirdwebProvider
-      desiredChainId={1}
-      authConfig={{
-        // Set this to your domain to prevent signature malleability attacks.
-        domain: "example.com",
-        authUrl: "/api/auth",
-      }}
-    >
+  
       <Component {...pageProps} />
-    </ThirdwebProvider>
+    
   );
 }
--- splitit/pages/index.tsx
+++ splitit/pages/index.tsx
@@ -5,6 +5,7 @@ import Navbar from '../components/Navbar'
 import Link from 'next/link'
 
 
+
 export default function Home() {
   return (
     <div className=''>
